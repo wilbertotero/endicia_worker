c4b18128633e8b87316f251276847079a813ee38
wilbertotero <wilbertotero@gmail.com>
2014-10-01 01:24:17 -0700

    Final test of the method

--- general.py
+++ general.py
@@ -1,46 +1,75 @@
+from SOAPpy import WSDL          
 import xmlrpclib
-from pydicia import *
 import time
-from xml.dom.minidom import parse, parseString
-import xml.etree.ElementTree as ET
 
+#
+#Method: transaction
+#Description: Test the if a package has been refunded
 
-#def status():
-proxy = xmlrpclib.ServerProxy("https://www.endicia.com/ELS/ELSServices.cfc")
-#count = 0
-#b = Batch()
-#b.etree
-b = ET.fromstring('''
-   <TransactionsListingRequest>
-        <AccountID></AccountID>
-        <PassPhrase></PassPhrase>
-        <Test>Y</Test>
-        <TransactionType></TransactionType>
-        <StartDate></StartDate>
-        <StartTime></StartTime>
-        <EndDate></EndDate>
-        <EndTime></EndTime>
-        <Results></Results>
-        <ToZipCode></ToZipCode>
-        <FromAddress></FromAddress>
-        <TrackingList>
-            <PICNumber>9400109699937126318047</PICNumber>
-            <PieceID></PieceID>
-            <CustomsID></CustomsID>
-        </TrackingList>
-    </TransactionsListingRequest>''')
-#while True:                        
-    #b.add_package(ToName('Some Body'),DAZzle.Test)
-    #print b.tostring()
-#    ++count
-value = proxy.GetTransactionsListing(b)
-#tree = ET.fromstring(value)
-print 'Status:',value.find('Status').text
-
-#    if count == 7:
-#        break
+def transaction():
+    wsdlFile = 'https://www.endicia.com/ELS/ELSServices.cfc?wsdl'
+    server = WSDL.Proxy(wsdlFile)    
+    count = 0;
     
-#    time.sleep(5)
+    # print server.methods.keys() 
+    # callInfo = server.methods['GetTransactionsListing']
+    # print callInfo.inparams
+    # print callInfo.inparams[0].name
+    # print callInfo.inparams[0].type
+    # 
+    # print callInfo.outparams
+    # print callInfo.outparams[0].name 
+    # print callInfo.outparams[0].type
+    
+    #The value 9400109699937126318047 was inserted by default but can be sent through the method
+    b = '''
+       <TransactionsListingRequest>
+            <AccountID></AccountID>
+            <PassPhrase></PassPhrase>
+            <Test>Y</Test>
+            <TransactionType></TransactionType>
+            <StartDate></StartDate>
+            <StartTime></StartTime>
+            <EndDate></EndDate>
+            <EndTime></EndTime>
+            <Results></Results>
+            <ToZipCode></ToZipCode>
+            <FromAddress></FromAddress>
+            <TrackingList>
+                <PICNumber>9400109699937126318047</PICNumber>
+                <PieceID></PieceID>
+                <CustomsID></CustomsID>
+            </TrackingList>
+        </TransactionsListingRequest>'''
+    
+    try:
+        while True:
+            count += 1
+            
+            xml = server.GetTransactionsListing(b)
+            #print xml
+            refund_code = xml['TransactionsListingResponse']['TransactionResults']['Transaction']['RefundCode']
+            
+            if refund_code in ['A','C', 'S', 'U', 'M']:
+                print 'Package has been refunded' + refund_code
+                break;
+            
+            elif refund_code == 'R':
+                print 'Package has been rejected' + refund_code
+            else:
+                print 'Package is pending for refund' + refund_code
+            
+            if count == 7: # testint the 7 times       
+                break
+            
+            time.sleep(1) #it can be changed to more second - just to see results of the exercise
+      
+    except xmlrpclib.ProtocolError as err:
+        print "A protocol error occurred"
+        print "URL: %s" % err.url
+        print "HTTP/HTTPS headers: %s" % err.headers
+        print "Error code: %d" % err.errcode
+        print "Error message: %s" % err.errmsg
 
-#status();
-
+transaction()
+    
